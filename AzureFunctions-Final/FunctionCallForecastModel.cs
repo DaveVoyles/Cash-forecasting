ce3febaf171ce5ca3e9556b43c0a1ac849f6b3a5
Dave Voyles <Dnvoyles@gmail.com>
2018-10-17 13:06:58 -0400

    Removed name from function

--- AzureFunctions-Final/FunctionCallForecastModel.cs
+++ AzureFunctions-Final/FunctionCallForecastModel.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 
-namespace FunctionsPNC
+namespace Functions
 {
     public static class FunctionCallForecastModel
     {
